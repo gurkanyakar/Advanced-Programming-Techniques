7338f9b05095f11a4284709add86117c1742debc
Gurkan Yakar <data.yakar@gmail.com>
2022-12-23 21:41:40 +0300

    add project files

---
+++ Project/assets/js/script.js
@@ -0,0 +1,99 @@
+let dataBase = [],
+  questionNumber = 0,
+  correctAnswers = 0,
+  limit = 4;
+let i, correct, countries;
+let container = document.getElementById("container");
+
+//sounds
+const correctAudioSound = new Audio("../assets/audio/correct.wav");
+const wrongAudioSound = new Audio("../assets/audio/wrong.wav");
+const finishAudioSound = new Audio("../assets/audio/finish.wav");
+
+fetch("https://restcountries.com/v2/all?fields=name,flag")
+  .then((data) => data.json())
+  .then((result) => {
+    dataBase = result;
+  })
+  .catch((error) => {
+    prev.innerHTML = error + " <br> Please try again later";
+    console.log(error);
+  });
+
+
+
+function rmv() {
+  container.innerHTML = "";
+}
+
+function correctAudio() {
+  correctAudioSound.play();
+}
+
+function wrongAudio() {
+  wrongAudioSound.play();
+}
+
+function finishAudio() {
+  finishAudioSound.play();
+}
+
+function flags() {
+  rmv();
+  questionNumber++;
+  i = Math.floor(Math.random() * dataBase.length);
+  countries = [dataBase[i].name];
+  correct = dataBase[i].name;
+
+  while (countries.length < limit) {
+    let choice = dataBase[Math.floor(Math.random() * dataBase.length)].name;
+    if (!countries.includes(choice)) {
+      countries.push(choice);
+    }
+  }
+
+  countries.sort();
+
+  container.innerHTML += `
+    <h1>Question number: ${questionNumber}</h1>
+    <h2>What country does this flag belongs to?</h2>
+    <img style="width:300px" src="${dataBase[i].flag}">
+    <div id="buttons"></div>`;
+
+  countries.map((country) => {
+    document.getElementById("buttons").innerHTML += `
+      <button onclick="testflag(\`${country}\`)">${country}</button>`;
+  });
+}
+
+function testflag(el) {
+  rmv();
+  if (el == correct) {
+    correctAudio();
+    correctAnswers++;
+    if (questionNumber % 10 != 0) {
+      flags();
+    } else {
+      finish();
+    }
+  } else {
+    wrongAudio();
+
+    if (questionNumber % 10 != 0) {
+      flags();
+    } else {
+      finish();
+    }
+  }
+}
+
+function finish() {
+  finishAudio();
+  container.innerHTML += `
+    <h1>Bravo!<br>
+    You finished the test!</h1>
+    <h2>Your Score is <b>${correctAnswers}/${questionNumber}</b></h2>
+    <button id="flags" onclick="flags()">Try Again</button>`;
+  questionNumber = 0;
+  correctAnswers = 0;
+}
